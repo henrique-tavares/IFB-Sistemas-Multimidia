bea336cc7e2861047efd3018223c539ea957fbb1
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2022-05-09 23:52:39 -0300

    restarting the game

---
+++ tulios-graveyard/src/game.ts
@@ -0,0 +1,22 @@
+import 'phaser';
+import Start from './scenes/start';
+
+const config = {
+  type: Phaser.AUTO,
+  backgroundColor: '#f9f9f9',
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 0,
+      },
+      debug: false,
+    },
+  },
+  width: 800,
+  height: 600,
+  scene: Start,
+};
+
+const game = new Phaser.Game(config);
